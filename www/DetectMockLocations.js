792debc72a57cb25bd0cc7ccdc799166726486fc
esantacruz <esantacruz@transportationamerica.com>
2019-10-30 13:11:11 -0500

    Fix 2

--- www/DetectMockLocations.js
+++ www/DetectMockLocations.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-module.exports.coolMethod = function(arg0, success, error) {
+module.exports.areThereMockPermissionApps = function(arg0, success, error) {
     exec(success, error, 'DetectMockLocations', 'areThereMockPermissionApps', [arg0]);
 };
