bc64c3956bebc3c579054492b5669214804024d3
esantacruz <esantacruz@transportationamerica.com>
2019-10-30 12:44:17 -0500

    first commit

---
+++ src/android/DetectMockLocations.java
@@ -0,0 +1,73 @@
+package cordova.detect.mock.location;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class DetectMockLocations extends CordovaPlugin {
+
+    private Context context;
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        context = this.cordova.getActivity();;
+        if (action.equals("areThereMockPermissionApps")) {
+            areThereMockPermissionApps(callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    public void areThereMockPermissionApps(CallbackContext callbackContext) {
+        int count = 0;
+
+        PackageManager pm = this.context.getPackageManager();
+        List<ApplicationInfo> packages =
+                pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        for (ApplicationInfo applicationInfo : packages) {
+            try {
+                PackageInfo packageInfo = pm.getPackageInfo(applicationInfo.packageName,
+                        PackageManager.GET_PERMISSIONS);
+
+                // Get Permissions
+                String[] requestedPermissions = packageInfo.requestedPermissions;
+
+                if (requestedPermissions != null) {
+                    for (int i = 0; i < requestedPermissions.length; i++) {
+                        if (requestedPermissions[i]
+                                .equals("android.permission.ACCESS_MOCK_LOCATION")
+                                && !applicationInfo.packageName.equals(context.getPackageName())) {
+                            count++;
+                        }
+                    }
+                }
+            } catch (PackageManager.NameNotFoundException e) {
+                Log.e("Got exception " , e.getMessage());
+            }
+        }
+        if (count > 0) {
+            callbackContext.success("Mock Location Is Enabled");
+        } else {
+            callbackContext.success("Mock Location Is Not Enabled");
+        }
+            
+    }
+
+
+}
